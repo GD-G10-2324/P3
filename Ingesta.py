26e85e5a9b9c9ef7f4f0e85fc1b948e180c3f7e3
Paula <Paula.Castillejo1@alu.uclm.es>
2024-03-19 11:37:31 +0100

    Límite de commits por ingesta en mongoDB impuesto
    Co-authored-by: RaulJDlCRUZ <Raul.Jimenez8@alu.uclm.es>
    Co-authored-by: Pablo Blázquez Sánchez <Pablo.Blazquez1@alu.uclm.es>

--- Ingesta.py
+++ Ingesta.py
@@ -1,44 +1,38 @@
 import requests
 import time
-import pymongo
+import math
 from urllib.parse import parse_qs, urlparse
 from pymongo import MongoClient
 
-# COMM_LIMIT = 5000
-# FAULT_DELTA = 20
-# SECONDSHOUR = 3600
+COMM_LIMIT = 5000
 
-# def get_commits_count(owner_name: str, repo_name: str) -> int:
-#     """
-#     Returns the number of commits to a GitHub repository.
-#     """
-#     rurl = 'https://api.github.com/repos/{}/{}/commits?since=2019-01-01&per_page=1'
-#     url= rurl.format(owner_name, repo_name)
-#     r = requests.get(url)
-#     links = r.links
-#     rel_last_link_url = urlparse(links["last"]["url"])
-#     rel_last_link_url_args = parse_qs(rel_last_link_url.query)
-#     rel_last_link_url_page_arg = rel_last_link_url_args["page"][0]
-#     commits_count = int(rel_last_link_url_page_arg)
-#     return commits_count
+def get_commits_count(owner_name: str, repo_name: str) -> int:
+    """
+    Returns the number of commits to a GitHub repository.
+    """
+    rurl = 'https://api.github.com/repos/{}/{}/commits?since=2019-01-01&per_page=1'
+    url= rurl.format(owner_name, repo_name)
+    r = requests.get(url)
+    links = r.links
+    rel_last_link_url = urlparse(links["last"]["url"])
+    rel_last_link_url_args = parse_qs(rel_last_link_url.query)
+    rel_last_link_url_page_arg = rel_last_link_url_args["page"][0]
+    commits_count = int(rel_last_link_url_page_arg)
+    return commits_count
 
 
-# username = "PAULACASTILLEJOBRAVO"
-# token = "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"
-# pabloToken = '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'
-# client_id = "3bab62c6318084a0999b"
+username = "PAULACASTILLEJOBRAVO"
+token = "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"
+client_id = "3bab62c6318084a0999b"
+client_secret = "9d5c1a69fb0c65cf45780164de266f100cd11d3c"
+# pabloUsername = "PabloBlazquezSanchez"
+# pabloToken = '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'
 # pabloClient_id = '32bfc97d07c905811c12'
-# client_secret = "9d5c1a69fb0c65cf45780164de266f100cd11d3c"
 # pabloClient_Secret = '047d8902e71e977fd711f9706bf0a6d95bc0d473'
 
-
-# contraseña atlas
-# usernameMongo = 'PAULACASTILLEJOBRAVO'
-# passwordMongo = 'KtBpNycnHOlwMwT7'
 headers = {'Authorization': 'Bearer '+token, 'Accept': 'application/vnd.github+json'}
 
 # MONGODB_HOST = f'mongodb+srv://{usernameMongo}:{passwordMongo}@cluster0.fus1bnz.mongodb.net/'
-# MONGODB_PORT = 27017
 MONGODB_HOST = 'localhost'
 MONGODB_PORT = 27017
 DB_NAME = 'github'
@@ -46,36 +40,27 @@ COLLECTION_COMMITS = 'commits'
 connection= MongoClient(MONGODB_HOST, MONGODB_PORT)
 collCommits= connection[DB_NAME][COLLECTION_COMMITS]
 repos_url= 'https://api.github.com/repos/{}/{}/commits?since=2019-01-01&page={}&per_page={}'
-#https://api.github.com/repos/sourcegraph/sourcegraph/commits?since=2019-01-01&page=1&per_page=1
-#https://api.github.com/rate_limit
-#usar core
+rate_url = 'https://api.github.com/rate_limit'
 user= 'sourcegraph'
 project= 'sourcegraph'
-# interval = round(SECONDSHOUR/(COMM_LIMIT-FAULT_DELTA),3)
-# totalcommits = get_commits_count(owner_name=user, repo_name=project)
-totalcommits = 5000
+max_pages = math.ceil((get_commits_count(owner_name=user, repo_name=project)/100))
 
-while totalcommits >= 0:
-    url= repos_url.format(user, project, totalcommits, 1)
-    rate_url = 'https://api.github.com/rate_limit'
-
+for page in range (1+max_pages): #range tiene de intervalo [0,range-1]
     query= {'client_id': client_id, 'client_secret': client_secret}
-    commits = requests.get(url, params=query, headers=headers)
-    rate =  requests.get(rate_url, params=query, headers=headers)
-    commits_dict = commits.json()
-    rate_dict = rate.json()
-    print(rate_dict)
-    for commit in commits_dict:
-        commit['projectId'] = project
-        print(str(commit))
-        collCommits.insert_one(commit)
-
-# time.sleep(interval) 
-    totalcommits-=1
-
-
-
-
+    rate_limit = requests.get(rate_url, params=query, headers=headers).json()['resources']['core']
+    print(rate_limit)
+    if rate_limit['remaining'] > 0:
+        # CONSULTA
+        url= repos_url.format(user, project, page, 100)
+        commits = requests.get(url, params=query, headers=headers)
+        commits_dict = commits.json()
+        for commit in commits_dict:
+            commit['projectId'] = project
+            # print(str(commit))
+            collCommits.insert_one(commit)
+    else:
+        delta = (rate_limit['reset']-time.time())/1000
+        time.sleep(delta)
 
 
 
